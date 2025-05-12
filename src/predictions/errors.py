6a34ed48d3df49b77f90f1e589f2f44400e0c064
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-13 00:43:49 +0100

    Update src/predictions/errors.py

--- src/predictions/errors.py
+++ src/predictions/errors.py
@@ -1,8 +1,10 @@
+"""Module errors.py"""
 
 import numpy as np
 import pandas as pd
 
 import src.elements.structures as st
+
 
 class Errors:
 
@@ -17,14 +19,23 @@ class Errors:
         :return:
         """
 
-        estimates = data[['median', 'lower_w', 'upper_w']].to_numpy()
+        # The error with respect to the median, for overarching error calculations
+        data.loc[:, 'error'] = data['median'] - data['observation']
 
-        data.loc[:, ['error', 'error_l', 'error_u']] = estimates - data['observation'].to_numpy()[:,None]
-        data['p_error'] = 100*np.true_divide(data['error'].to_numpy(), data['observation'].to_numpy())
+        # Calculating error percentages
+        estimates = data[['median', 'lower_w', 'upper_w']].to_numpy()
+        raw = estimates - data['observation'].to_numpy()[:,None]
+        percentages = 100*np.true_divide(raw, data['observation'].to_numpy()[:,None])
+        data.loc[:, ['p_error', 'p_e_lower_w', 'p_e_upper_w']] = percentages
 
         return data
 
     def exc(self, structures: st.Structures):
+        """
+
+        :param structures:
+        :return:
+        """
 
         structures = structures._replace(
             training=self.__get_errors(data=structures.training),
