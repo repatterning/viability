0ae2735b1fbb5b1dd73b9e04c41adf4429612f26
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-13 02:08:50 +0100

    Update src/predictions/structures.py

--- src/predictions/structures.py
+++ src/predictions/structures.py
@@ -1,3 +1,4 @@
+"""Module structures.py"""
 import pandas as pd
 
 import src.elements.master as mr
@@ -5,8 +6,16 @@ import src.elements.structures as st
 
 
 class Structures:
+    """
+    Builds the training, testing, and futures data structures
+    """
 
     def __init__(self, master: mr.Master, arguments: dict):
+        """
+
+        :param master:
+        :param arguments:
+        """
 
         self.__master = master
         self.__arguments = arguments
@@ -18,6 +27,10 @@ class Structures:
         self.__estimates = estimates.sort_values(by='date', ascending=True, inplace=False)
 
     def __training(self):
+        """
+
+        :return:
+        """
 
         training = self.__master.training
         training['date'] = pd.to_datetime(training['timestamp'], unit='ms')
@@ -27,6 +40,10 @@ class Structures:
         return data
 
     def __testing(self):
+        """
+
+        :return:
+        """
 
         testing = self.__master.testing
         testing['date'] = pd.to_datetime(testing['timestamp'], unit='ms')
@@ -36,10 +53,18 @@ class Structures:
         return data
 
     def __futures(self):
+        """
+
+        :return:
+        """
 
         return self.__estimates[-self.__arguments.get('ahead'):]
 
     def exc(self) -> st.Structures:
+        """
+
+        :return:
+        """
 
         return st.Structures(
             training=self.__training(), testing=self.__testing(), futures=self.__futures())
