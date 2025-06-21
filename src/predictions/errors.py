94f78967f9e1b4c3337a3f192b6bef442ab9b86d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-21 13:59:34 +0100

    Update src/predictions/errors.py

--- src/predictions/errors.py
+++ src/predictions/errors.py
@@ -26,10 +26,10 @@ class Errors:
         data.loc[:, 'error'] = data['median'] - data['observation']
 
         # Calculating error percentages
-        estimates = data[['median', 'lower_w', 'upper_w']].to_numpy()
+        estimates = data[['median', 'lower_w', 'upper_w', 'lower_q', 'upper_q']].to_numpy()
         raw = estimates - data['observation'].to_numpy()[:,None]
         percentages = 100*np.true_divide(raw, data['observation'].to_numpy()[:,None])
-        data.loc[:, ['p_error', 'p_e_lower_w', 'p_e_upper_w']] = percentages
+        data.loc[:, ['p_error', 'p_e_lower_w', 'p_e_upper_w', 'p_e_lower_q', 'p_e_upper_q']] = percentages
 
         return data
 
